3b9c7a2684bf6b2e481108e37f2fcd3b8723dfbb
carlyle-felix <carlyle.felix@gmail.com>
2024-10-16 23:33:25 +0200

    add:   difficulty level 4:  Operand 'x' and unkown operator

--- quiz.c
+++ quiz.c
@@ -1,3 +1,6 @@
+ // Author: Carlyle Felix
+ // This math quiz was written with the basics I've learned to date.
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -11,13 +14,13 @@ int main(void) {
 
     srand((unsigned) time(NULL));
 
-    printf("difficulty:\n(1) Operands < 10\n(2) Operands < 100\n(3) One operand is x\nSelect:  ");
+    printf("difficulty:\n(1) Operands < 10\n(2) Operands < 100\n(3) One operand is x and operands < 10\n(4) One operator is x, operand unkown and operands < 100\nSelect:  ");
     scanf("%d", &lvl);
 
-    if (lvl == 1) {
-        digits = 9;
-    } else if (lvl == 2 || lvl == 3) {
-        digits = 99;
+    if (lvl == 1 || lvl == 3) {                                                                 // digits should never be zero, add 1 when calling rand()
+        digits = 8;
+    } else if (lvl == 2 || lvl == 4) {
+        digits = 98;
     } else {
         printf("We're not there yet.\n");
         return 0;
@@ -28,16 +31,16 @@ int main(void) {
 
     for (round = 0; round < n; round++) {
         sym = rand() % 4;
-        num1 = rand() % digits;
-        num2 = rand() % digits;
-        
-        if (sym == 0 && lvl == 2 || sym == 0 && lvl == 3)                                       // Multiplication: for lvl 2, Make num2 a single digit
+        num1 = rand() % digits + 1;
+        num2 = rand() % digits + 1;
+
+        // operator specifics
+        if (sym == 0 && lvl > 1)                                                                // Multiplication: for levels 2, 3 and 4: Make num2 a single digit
             num2 = num2 % 10;
         else if (sym == 1) {                                                                   // Division: Make num1 % num2 == 0
             for (i = 0; num1 % num2 != 0 && i < 5; i++) {
-                num1 = rand() % digits;
-                num2 = rand() % digits;
-
+                num1 = rand() % digits + 1;
+                num2 = rand() % digits + 1;
                 if (num1 < num2) {
                     temp = num1;
                     num1 = num2;
@@ -51,21 +54,45 @@ int main(void) {
             }
         }
 
-        if (lvl == 3) {
+        // level specifics
+        if (lvl > 2) {
             quiz_ans = calc(num1, num2, sym);
-            
-            if (rand() % 2 == 0) {
-                printf("\n%d.  x %c %d = %d\n", round + 1, operator(sym), num2, quiz_ans);
-                printf("    x = ");
-                scanf("%d", &num1);
-            } else {
-                num2 = 'x';    
-                printf("\n%d.  %d %c x = %d\n", round + 1, num1, operator(sym), quiz_ans);
-                printf("    x = ");
-                scanf("%d", &num2);
-            }
-            user_ans = calc(num1, num2, sym);
+            getchar();                                                                          // Dispose '\n'
+            switch (lvl) {
+            case 3:
+                if (rand() % 2 == 0) {
+                    printf("\n%d.  x %c %d = %d\n", round + 1, operator(sym), num2, quiz_ans);
+                    printf("    x = ");
+                    scanf("%d", &num1);
+                } else {
+                    num2 = 'x';    
+                    printf("\n%d.  %d %c x = %d\n", round + 1, num1, operator(sym), quiz_ans);
+                    printf("    x = ");
+                    scanf("%d", &num2);
+                }
+                user_ans = calc(num1, num2, sym);
+                break;
 
+            case 4:
+                if (rand() % 2 == 0) {
+                    printf("\n%d.  x ? %d = %d\n", round + 1, num2, quiz_ans);
+                    printf("    x = ");
+                    scanf("%d", &num1);
+                    getchar();                                                                  // Dispose '\n'
+                    printf("    Operator: ");
+                    sym = (int) getchar();
+                } else {
+                    num2 = 'x';    
+                    printf("\n%d.  %d ? x = %d\n", round + 1, num1, quiz_ans);
+                    printf("    Operator: ");
+                    sym = (int) getchar();
+                    printf("    x = ");
+                    scanf("%d", &num2);
+                }
+                
+                user_ans = calc(num1, num2, sym);
+                break;
+            }       
         } else {
             printf("\n%d.  %d %c %d = ", round + 1, num1, operator(sym), num2);
             scanf("%d", &user_ans);
@@ -106,16 +133,16 @@ void grade(float total) {
     }
 }
 
-int calc(int num1, int num2, int operator) {
+int calc(int num1, int num2, int operator) {                                                    // ASCII conversions
 
     switch (operator) {
-    case 0:
+    case 0: case 42:
         return num1 * num2;
-    case 1:
+    case 1: case 47:
         return num1 / num2;
-    case 2:
+    case 2: case 43:
         return num1 + num2;
-    case 3:
+    case 3: case 45:
         return num1 - num2;
     }
 }
