0190df813f1350789e8287c4e75959ec12899fe3
carlyle-felix <carlyle.felix@gmail.com>
2024-10-16 03:49:11 +0200

    mitigate repetition in main()
    modified and tested on mobile.

--- quiz.c
+++ quiz.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+char operator(int operator);
 void grade(float total);
+bool evaluate(int num1, int num2, int operator);
 
 int main(void) {
-    int num1, num2, ans, digits, lvl, op, round, n, score = 0, temp, i;                      
+    int num1, num2, digits, lvl, op, round, n, score = 0, temp, i, result;
 
     srand((unsigned) time(NULL));
 
@@ -19,38 +22,14 @@ int main(void) {
     printf("Enter number of rounds: ");
     scanf("%d", &n);
 
-    
     for (round = 0; round < n; round++) {
         op = rand() % 4;
         num1 = rand() % digits;
         num2 = rand() % digits;
-        if (op == 0) {                                              // Addition
-            printf("%d + %d =   ", num1, num2);
-            scanf("%d", &ans);
-            if (ans == num1 + num2){
-                printf("Correct!\n");
-                score++;
-            } else 
-                printf("Incorrect, don't give up!\n");
-        } else if (op == 1) {                                       // Subtraction
-            printf("%d - %d =   ", num1, num2);
-            scanf("%d", &ans);
-            if (ans == num1 - num2){
-                printf("Correct!\n");
-                score++;
-            } else 
-                printf("Incorrect, don't give up!\n");
-        } else if (op == 2) {                                       // Multiplication: for lvl 2, Make num2 a single digit
-            if (lvl == 2) 
-               num2 = num2 % 10;
-            printf("%d x %d =   ", num1, num2);
-            scanf("%d", &ans);
-            if (ans == num1 * num2){
-                printf("Correct!\n");
-                score++;
-            } else 
-                printf("Incorrect, don't give up!\n");
-        } else if (op == 3) {                                        // Division: Make num1 % num2 == 0
+        
+        if (op == 0 && lvl == 2)                                       // Multiplication: for lvl 2, Make num2 a single digit
+            num2 = num2 % 10;
+        else if (op == 1) {                                            // Division: Make num1 % num2 == 0
             for (i = 0; num1 % num2 != 0 && i < 5; i++) {
                 num1 = rand() % digits;
                 num2 = rand() % digits;
@@ -66,35 +45,78 @@ int main(void) {
                 round--;
                 continue;
             }
-
-            printf("%d / %d =   ", num1, num2);
-            scanf("%d", &ans);
-            if (ans == num1 / num2){
-                printf("Correct!\n");
-                score++;
-            } else 
-                printf("Incorrect, don't give up!\n");
         }
-    }
-    printf("\nYou got %d out of %d\n", score, n);
+        printf("%d %c %d = ", num1, operator(op), num2);
+        result = evaluate(num1, num2, op);
+        if (result)
+            score++;
+    }        
+    printf("\nYou got %d out of %d.\n", score, n);
     grade((float) score / n * 100);
-    
 }
 
 void grade(float total) {
 
-    printf("\nGrade:  ");
     switch ((int) total / 10) {
-        case 10:    printf("Perfect, A!\n");
-                    break;
-        case 9:     printf("A\n");
-                    break;
-        case 8:     printf("B\n");
-                    break;
-        case 7:     printf("C\n");
-                    break;
-        case 6:     printf("D\n");
-                    break;
-        default:    printf("F\n");  
+    case 10:
+        printf("Grade: Perfect, A!\n");
+        break;
+    case 9:
+        printf("Grade: A\n");
+        break;
+    case 8:
+        printf("Grade: B\n");
+        break;
+    case 7:
+        printf("Grade: C\n");
+        break;
+    case 6:
+        printf("Grade: D\n");
+        break;
+    default:
+        printf("Grade: F\n");
+    }
+}
+
+bool evaluate(int num1, int num2, int operator) {
+    
+    int quiz_ans, user_ans;
+    
+    switch (operator) {
+    case 0:
+        quiz_ans = num1 * num2;
+        break;
+    case 1:
+        quiz_ans = num1 / num2;
+        break;
+    case 2:
+        quiz_ans = num1 + num2;
+        break;
+    case 3:
+        quiz_ans = num1 - num2;
+        break;
+    }
+    
+    scanf("%d", &user_ans);
+    if (quiz_ans == user_ans) {
+        printf("Correct!\n");
+        return true;
+    } else {
+        printf("Incorrect, don't give up!\n");
+        return false;
+    }
+}
+
+char operator(int operator) {
+
+    switch (operator) {
+    case 0:
+        return 'x';
+    case 1:
+        return '/';
+    case 2:
+        return '+';
+    case 3:
+        return '-';
     }
 }
